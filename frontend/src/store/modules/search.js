271dead2a5e056e4b6eef861e4c5dfef1f98e0d1
study2895 <144806675+study2895@users.noreply.github.com>
2024-12-15 20:48:49 +0900

    Design: 새로고침 버튼 오류 수정

--- frontend/src/store/modules/search.js
+++ frontend/src/store/modules/search.js
@@ -1,8 +1,7 @@
-// store/search.js
 export const search = {
   namespaced: true,
   state: () => ({
-    results: [] // 검색 결과를 저장하는 상태
+    results: []
   }),
   mutations: {
     SET_RESULTS(state, payload) {
